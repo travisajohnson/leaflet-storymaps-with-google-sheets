0a6b75693333a71f95e62067bc93c798e422b19c
travisajohnson <tjohns76@icloud.com>
2017-12-20 20:25:53 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1lV39pU5kRDgMEyUFVxUGaB4CUL-_2eO84ipUfFM4UTo/edit#gid=0/';
